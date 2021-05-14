6695d346416660efa320426871f931857fa61cb4
Nate Hedgeman <47337337+hedgemanNate@users.noreply.github.com>
2021-05-14 02:40:24 -0400

    added textViewBackground color

--- Helpers/CustomColors.swift
+++ Helpers/CustomColors.swift
@@ -16,5 +16,6 @@ class CustomColors: UIColor {
     let secondaryColorPurple = UIColor(red: 172/255, green: 155/255, blue: 206/255, alpha: 1)
     let typingTextColorPurple = UIColor(red: 81/255, green: 60/255, blue: 105/255, alpha: 1)
     let alertBorderColor = UIColor(red: 217/255, green: 210/255, blue: 229/255, alpha: 1)
+    let textViewBackground = UIColor(red: 220/255, green: 213/255, blue: 242/255, alpha: 1)
 }
 
