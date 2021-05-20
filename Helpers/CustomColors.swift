31356b7f64bae9da222c7886aa8f69135d5828c2
Nate Hedgeman <47337337+hedgemanNate@users.noreply.github.com>
2021-05-20 13:40:15 -0400

    added navigationBGPurple and navigationTextBlue

--- Helpers/CustomColors.swift
+++ Helpers/CustomColors.swift
@@ -14,6 +14,10 @@ class CustomColors: UIColor {
     
     let mainColorPurple = UIColor(red: 151/255, green: 140/255, blue: 175/255, alpha: 1)
     let secondaryColorPurple = UIColor(red: 172/255, green: 155/255, blue: 206/255, alpha: 1)
+    
+    let navigationBGPurple = UIColor(red: 172/255, green: 155/255, blue: 206/255, alpha: 1)
+    let navigationTextBlue = UIColor(red: 56/255, green: 79/255, blue: 104/255, alpha: 1)
+    
     let typingTextColorPurple = UIColor(red: 81/255, green: 60/255, blue: 105/255, alpha: 1)
     let alertBorderColor = UIColor(red: 217/255, green: 210/255, blue: 229/255, alpha: 1)
     let textViewBackground = UIColor(red: 220/255, green: 213/255, blue: 242/255, alpha: 1)
