d0e66f2a5bcede3eadde7566b18f7e3f67a4bcb5
Nate Hedgeman <47337337+hedgemanNate@users.noreply.github.com>
2021-07-31 01:37:42 -0400

    added class to handle all time related things. Need to add the slide show timers here as well

--- Helpers/TimeController.swift
+++ Helpers/TimeController.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+import Firebase
+
+class TimeController {
+    var todayString = ""
+    var thisYear = ""
+    
+    let dateFormatter = DateFormatter() 
+    let dayMonthDayYearTime = "E, MMMM d, yyyy ha" //1
+    let dayMonthDay = "E, MMMM d"
+    let monthDayYearTime = "MMMM d, yyyy ha" //2
+    let monthDayYear = "MMMM d, yyyy" //3
+    
+    let year = "yyyy"
+    
+    let now = Date()
+    let inFourHours = Date().addingTimeInterval(14400)
+    let fourHoursAgo = Date().addingTimeInterval(-14400)
+    
+    
+    func setTime() {
+        //For todayString
+        dateFormatter.dateFormat = monthDayYear
+        todayString = dateFormatter.string(from: now)
+            //Manual Date
+        todayString = "July 23, 2021"
+        
+        //For thisYear
+        dateFormatter.dateFormat = year
+        thisYear = dateFormatter.string(from: now)
+    }
+}
+
+let timeController = TimeController()
