9db3964348ca56dc2dcdb23395ea4b917c8f5e03
Nate Hedgeman <47337337+hedgemanNate@users.noreply.github.com>
2021-07-31 01:34:09 -0400

    removed optional from onHold property

--- Ohmicity/Models/Show.swift
+++ Ohmicity/Models/Show.swift
@@ -15,7 +15,7 @@ struct Show: Codable, Equatable, Hashable {
     var dateString: String
     var date = Date()
     var time = ""
-    var onHold: Bool? //To removed shows from user cache
+    var onHold: Bool = false //To removed shows from user cache
     var ohmPick: Bool = false
     
     //Equatable Conformity
