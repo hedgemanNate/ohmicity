2d0b3889660b05a315cfda713036b76127252a90
Nate Hedgeman <47337337+hedgemanNate@users.noreply.github.com>
2021-08-13 02:00:50 -0400

    Created new data models that will hold more information: xityband xitybusiness along with xityshow, rewrote the venueDetailedViewController to work with new data models, compiled new data models in the loadInitDataViewController, add hashing abilities to old data models and enums

---
+++ Ohmicity/Controllers/Model Controllers/XityBusinessController.swift
@@ -0,0 +1,8 @@
+//
+//  XityBusinessController.swift
+//  Ohmicity
+//
+//  Created by Nathan Hedgeman on 8/12/21.
+//
+
+import Foundation
---
+++ Ohmicity/Models/XityBusiness.swift
@@ -0,0 +1,8 @@
+//
+//  XityBusiness.swift
+//  Ohmicity
+//
+//  Created by Nathan Hedgeman on 8/12/21.
+//
+
+import Foundation
