1eceab8aecf6810a83b7f3c549a22b12cf7211ca
Nate Hedgeman <47337337+hedgemanNate@users.noreply.github.com>
2021-09-06 18:07:34 -0400

    added business name to top of business detail screen and made the top logo square.

--- Ohmicity/Controllers/VenueDetailViewController.swift
+++ Ohmicity/Controllers/VenueDetailViewController.swift
@@ -39,6 +39,8 @@ class VenueDetailViewController: UIViewController {
     @IBOutlet weak var listenButton: UIButton!
     
     @IBOutlet weak var businessLogoImageView: UIImageView!
+    @IBOutlet weak var businessNameLabel: UILabel!
+    
     @IBOutlet weak var bandPhotoImageView: UIImageView!
     
     @IBOutlet weak var tonightsEntLabel: UILabel!
@@ -205,7 +207,7 @@ extension VenueDetailViewController {
     @objc private func updateViews() {
         guard let xityBusiness = xityBusiness else { return NSLog("No Current Business Found: updateViews: venueDetailViewController")}
         
-        let businessLogoData = xityBusiness.business.logo 
+        let businessLogoData = xityBusiness.business.logo
         
         if currentUserController.currentUser == nil {
             recommendButton.isEnabled = false
@@ -215,11 +217,13 @@ extension VenueDetailViewController {
             recommendButton.setTitle("Recommend", for: .normal)
         }
         
+        
         //SetTime
         timeController.dateFormatter.dateFormat = timeController.monthDayYear
         
-        //Banner Photo Adjustments
-        businessLogoImageView.layer.cornerRadius = businessLogoImageView.frame.height / 2
+        //Banner Photo/Info Adjustments
+        //businessLogoImageView.layer.cornerRadius = businessLogoImageView.frame.height / 2
+        businessNameLabel.text = xityBusiness.business.name
         
         //Top Buttons UI -
                 //This sets the hearts sizes programmatically
